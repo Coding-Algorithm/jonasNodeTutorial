02f2b6c47373870cb17cdea7a28f42e9c2af6df8
ibrahim taofeek opeyemi <taofeekibrahimope@gmail.com>
2023-04-19 10:49:52 +0100

    new additions"

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const morgan = require('morgan');
 
-const tourRouter = require('./routes/tourRoutes')
-const userRouter = require('./routes/userRoutes')
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
 // 1) MIDDLEWARES
-
-app.use(morgan('dev'));
-
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware');
@@ -25,10 +26,5 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-
-
 // 4)   SERVER
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
-});
+module.exports = app;
