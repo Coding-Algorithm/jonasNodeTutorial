ee680e7e90bb74b8d7957237f1aae310f9563c9c
ibrahim taofeek opeyemi <taofeekibrahimope@gmail.com>
2023-04-22 17:57:59 +0100

    new changes

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const app = require('./app');
-
 dotenv.config({ path: './config.env' });
+const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
@@ -22,3 +21,4 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
+
